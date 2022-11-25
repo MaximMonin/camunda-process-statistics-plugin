c145aafdb5724e676f91e61e8dcfb709fd87750f
Maxim Monin <maximmonin@gmail.com>
2022-11-25 18:13:05 +0000

    fixed issue with days + time in milliseconds

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,15 +1,17 @@
 export const asctime = (duration: number): string => {
-  const milliseconds = parseInt(`${(duration % 1000) / 10}`, 10),
+  const milliseconds = Math.floor(duration % 1000),
     seconds = Math.floor((duration / 1000) % 60),
     minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24),
+    days = Math.floor(duration / (1000 * 60 * 60 * 24));
 
   const hours_ = hours < 10 ? '0' + hours : hours;
   const minutes_ = minutes < 10 ? '0' + minutes : minutes;
   const seconds_ = seconds < 10 ? '0' + seconds : seconds;
-  const milliseconds_ = milliseconds < 10 ? '0' + milliseconds : milliseconds;
+  let milliseconds_ = milliseconds < 10 ? '0' + milliseconds : milliseconds;
+  milliseconds_ = milliseconds < 100 ? '0' + milliseconds_ : milliseconds_;
 
-  return hours_ + ':' + minutes_ + ':' + seconds_ + '.' + milliseconds_;
+  return days + ' ' + hours_ + ':' + minutes_ + ':' + seconds_ + '.' + milliseconds_;
 };
 
 export const filter: <T>(iterable: T[], condition: (x: T) => boolean) => T[] = (iterable, condition) => {
